e759126cddcc82dd33c8695c27c235e4cab492b2
froop <frooprogrammer@gmail.com>
2014-05-15 20:23:24 +0900

    add SampleResource#getList()

--- src/main/java/froop/rest/resource/SampleResource.java
+++ src/main/java/froop/rest/resource/SampleResource.java
@@ -5,6 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("samples")
@@ -14,6 +17,12 @@ public class SampleResource {
   @Path("text")
   public String getText() {
     return "dummy text";
+  }
+
+  @GET
+  @Produces(APPLICATION_JSON)
+  public List<SampleBean> getList() {
+    return Arrays.asList(new SampleBean("1"), new SampleBean("2"));
   }
 
   @GET
