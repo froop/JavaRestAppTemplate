54530392d27cf399f9587af1910ee78393eb304d
froop <frooprogrammer@gmail.com>
2014-05-15 21:34:24 +0900

    refactoring use lombok

--- src/main/java/froop/rest/resource/SampleBean.java
+++ src/main/java/froop/rest/resource/SampleBean.java
@@ -1,17 +1,12 @@
 package froop.rest.resource;
 
+import lombok.Data;
+
+@Data
 public class SampleBean {
   private String id;
 
   public SampleBean(String id) {
-    this.id = id;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
     this.id = id;
   }
 }
