d8020b125aea9ec050a43551bf503670a2f9647b
froop <frooprogrammer@gmail.com>
2014-05-15 17:06:04 +0900

    add SampleResource#getItem()

---
+++ src/main/java/froop/rest/resource/SampleBean.java
@@ -0,0 +1,17 @@
+package froop.rest.resource;
+
+public class SampleBean {
+  private String id;
+
+  public SampleBean(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+}
--- src/main/java/froop/rest/resource/SampleResource.java
+++ src/main/java/froop/rest/resource/SampleResource.java
@@ -2,12 +2,24 @@ package froop.rest.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
-@Path("sample")
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("samples")
 public class SampleResource {
 
   @GET
-  public String getDummy() {
-    return "dummy";
+  @Path("text")
+  public String getText() {
+    return "dummy text";
+  }
+
+  @GET
+  @Path("{id}")
+  @Produces(APPLICATION_JSON)
+  public SampleBean getItem(@PathParam("id") String id) {
+    return new SampleBean(id);
   }
 }
